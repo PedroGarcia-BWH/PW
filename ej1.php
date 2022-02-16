c9295dccab1427b2b3d96e89e7f7cbcdb0692d36
Saikora <pedro.garciaromera@alum.uca.es>
2022-02-16 10:37:44 +0100

    ej1

---
+++ ej1.php
@@ -0,0 +1,15 @@
+<hmtl>
+
+<head>ej1<title></title></head>
+<body>
+    <h1>Ejercicio 1</h1>
+    <?php 
+    $var1 = "Hola";
+    $var2 = "Mundo";
+    
+    print "$var1.$var2";
+    ?>
+</body>
+</hmtl>
+
+
