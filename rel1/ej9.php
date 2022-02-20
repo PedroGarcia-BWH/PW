922ca855b74a7c03db077aa006a1010b299d3830
Saikora <pedro.garciaromera@alum.uca.es>
2022-02-20 19:14:08 +0100

    modificacion

---
+++ rel1/ej9.php
@@ -0,0 +1,16 @@
+<html>
+
+<head>Ej4<title></title></head>
+<body>
+    <h1>Ejercicio 4</h1>
+    <?php 
+    $contador = 1;
+    echo "<table>";
+    
+    $ruta = "img/f"
+        echo "<tr>";
+    
+    echo "</table>";
+    ?>
+</body>
+</html>
